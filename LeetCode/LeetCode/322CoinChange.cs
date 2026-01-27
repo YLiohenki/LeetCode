3cc7c1308bac5151098e8e2b4bce0edc1ec9c56b
yliohenkitransperfect <yliohenki@transperfect.com>
2026-01-27 14:08:59 +0100

    #322 | feat: coin change solution

---
+++ LeetCode/LeetCode/322CoinChange.cs
@@ -0,0 +1,30 @@
+﻿public class Solution322
+{
+
+    public int CoinChange(int[] coins, int amount)
+    {
+        if (amount == 0)
+            return 0;
+        if (coins.Length == 0)
+            return -1;
+
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Array.Fill(dp, max);
+        dp[0] = 0;
+
+
+        for (long i = 0; i <= amount; i++)
+        { 
+            foreach (long coin in coins)
+            {
+                if (i + coin <= amount && dp[i + coin] > dp[i] + 1)
+                {
+                    dp[i + coin] = dp[i] + 1;
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var readLine = Console.ReadLine();
+int[] coins = readLine.Split(' ').Select(s => int.Parse(s)).ToArray();
+var amount = int.Parse(Console.ReadLine());
+
+var solution322 = new Solution322();
+
+var result = solution322.CoinChange(coins, amount);
+
+Console.WriteLine(result);
