ca51cf9d30f7ac8b65fc6cb72ed4bbfd8d7809cf
Yauheni Liohenki <legenkiyes@gmail.com>
2026-02-06 10:36:31 +0100

    295FindMedianfromDataStream
    295FindMedianfromDataStream

---
+++ LeetCode/LeetCode/295FindMedianfromDataStream.cs
@@ -0,0 +1,53 @@
+﻿public class MedianFinder
+{
+    PriorityQueue<int, int> biggerNumbers;
+    PriorityQueue<int, int> smallerNumbers;
+
+    public MedianFinder()
+    {
+        biggerNumbers = new ();
+        smallerNumbers = new ();
+    }
+
+    public void AddNum(int num)
+    {
+        if (biggerNumbers.TryPeek(out int bigElement, out int bigPriority) && bigPriority < num)
+        {
+            biggerNumbers.Enqueue(num, num);
+        }
+        else
+        {
+            smallerNumbers.Enqueue(num, -num);
+        }
+
+        if (smallerNumbers.Count > biggerNumbers.Count + 1)
+        {
+            var element = smallerNumbers.Dequeue();
+            biggerNumbers.Enqueue(element, element);
+        }
+
+        if (biggerNumbers.Count > smallerNumbers.Count)
+        {
+            var element = biggerNumbers.Dequeue();
+            smallerNumbers.Enqueue(element, -element);
+        }
+    }
+
+    public double FindMedian()
+    {
+        var smallElement = smallerNumbers.Peek();
+        if (smallerNumbers.Count > biggerNumbers.Count)
+        {
+            return smallElement;
+        }
+        var bigElement = biggerNumbers.Peek();
+        return (smallElement + bigElement) / 2.0;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.AddNum(num);
+ * double param_2 = obj.FindMedian();
+ */
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,12 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var readLine = Console.ReadLine();
-int[] coins = readLine.Split(' ').Select(s => int.Parse(s)).ToArray();
-var amount = int.Parse(Console.ReadLine());
+var MedianFinder = new MedianFinder();
 
-var solution322 = new Solution322();
+MedianFinder.AddNum(1);
+MedianFinder.AddNum(2);
 
-var result = solution322.CoinChange(coins, amount);
+Console.WriteLine(MedianFinder.FindMedian());
+MedianFinder.AddNum(3);
+Console.WriteLine(MedianFinder.FindMedian());
 
-Console.WriteLine(result);
+Console.ReadKey();
