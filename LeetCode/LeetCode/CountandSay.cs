fb557ac3b0b1cf4f76f2dfb41bb0eef6c158696b
yliohenkitransperfect <yliohenki@transperfect.com>
2026-02-16 17:27:44 +0100

    Count and say , Product of array except self

---
+++ LeetCode/LeetCode/CountandSay.cs
@@ -0,0 +1,28 @@
+﻿public class CountAndSayClass
+{
+    public string CountAndSay(int n)
+    {
+        if (n == 1)
+            return "1";
+        var prev = this.CountAndSay(n - 1);
+        var result = "";
+        var count = 1;
+        var ch = prev[0];
+        for (int i = 1; i < prev.Length; ++i)
+        {
+            if (prev[i] == ch)
+            {
+                count++;
+            }
+            else
+            {
+                result += count.ToString() + ch;
+                ch = prev[i];
+                count = 1;
+            }
+        }
+
+        result += count.ToString() + ch;
+        return result;
+    }
+}
---
+++ LeetCode/LeetCode/ProductofArrayExceptSelf.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class ProductofArrayExceptSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] leftMulti = new int[nums.Length];
+            int[] rightMulti = new int[nums.Length];
+            leftMulti[0] = nums[0];
+            rightMulti[nums.Length - 1] = nums[nums.Length - 1];
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                leftMulti[i] = nums[i] * leftMulti[i - 1];
+                rightMulti[nums.Length - i - 1] = nums[nums.Length - i - 1] * rightMulti[nums.Length - i];
+            }
+            var result = new int[nums.Length];
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                result[i] = (i > 0 ? leftMulti[i - 1] : 1) * (i < nums.Length - 1 ? rightMulti[i + 1] : 1);
+            }
+            return result;
+        }
+    }
+}
