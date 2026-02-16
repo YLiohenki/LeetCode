eb2bf06ff1c361ed85aa6c39ec4d0114863deebb
yliohenkitransperfect <yliohenki@transperfect.com>
2026-02-16 15:57:39 +0100

    feat: 91. Decode Ways

---
+++ LeetCode/LeetCode/91DecodeWays.cs
@@ -0,0 +1,34 @@
+﻿public class DecodeWays
+{
+    public int NumDecodings(string s)
+    {
+        if (s.Length == 0 || s == "0" || s[0] == '0')
+            return 0;
+        var ways = new int[s.Length];
+        ways[0] = 1;
+        var supportedWith2 = new int[] { '1', '2', '3', '4', '5', '6' };
+        for (int i = 1; i < s.Length; ++i)
+        {
+            if (s[i] == '0' && s[i - 1] == '0')
+                return 0;
+            else if (s[i - 1] == '1' && s[i] != '0')
+            {
+                ways[i] = ways[i - 1] + ways[Math.Max(i - 2, 0)];
+            }
+            else if (supportedWith2.Contains(s[i]) && s[i - 1] == '2')
+            {
+                ways[i] = ways[i - 1] + ways[Math.Max(i - 2, 0)];
+            }
+            else if (s[i] == '0')
+            {
+                if (s[i - 1] > '2')
+                    return 0;
+                ways[i] = ways[Math.Max(i - 2, 0)];
+            }
+            else {
+                ways[i] = ways[i - 1];
+            }
+        }
+        return ways[s.Length - 1];
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,13 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var MedianFinder = new MedianFinder();
+//var MedianFinder = new MedianFinder();
+//
+//MedianFinder.AddNum(1);
+//MedianFinder.AddNum(2);
+//
+//Console.WriteLine(MedianFinder.FindMedian());
+//MedianFinder.AddNum(3);
+//Console.WriteLine(MedianFinder.FindMedian());
 
-MedianFinder.AddNum(1);
-MedianFinder.AddNum(2);
+var DecodeWays = new DecodeWays();
 
-Console.WriteLine(MedianFinder.FindMedian());
-MedianFinder.AddNum(3);
-Console.WriteLine(MedianFinder.FindMedian());
+var str = Console.ReadLine();
+
+var number = DecodeWays.NumDecodings(str);
+
+Console.WriteLine(number);
 
 Console.ReadKey();
