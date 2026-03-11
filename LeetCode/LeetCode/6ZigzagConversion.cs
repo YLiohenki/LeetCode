14c0fddedfa59f34ebfdfa92b71a6fdb9c6b70b5
yliohenkitransperfect <yliohenki@transperfect.com>
2026-03-11 14:49:41 +0100

    6. Zigzag Conversion

---
+++ LeetCode/LeetCode/6ZigzagConversion.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    public class Solution6
+    {
+        public string Convert(string s, int numRows)
+        {
+            int patLength = numRows + Math.Max(0, numRows - 2);
+            var result = new StringBuilder();
+            for (int rowNum = 0; rowNum < numRows; ++rowNum)
+            {
+                int index = 0;
+                while (index < s.Length)
+                {
+                    if (index + rowNum < s.Length)
+                    {
+                        result.Append(s[index + rowNum]);
+                    }
+                    if (rowNum > 0 && rowNum < numRows - 1)
+                    {
+                        var toAddIndex = index + (numRows - 1) + (numRows - 2) - (rowNum - 1);
+                        if (toAddIndex < s.Length)
+                        {
+                            result.Append(s[toAddIndex]);
+                        }
+                    }
+                    index += patLength;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
